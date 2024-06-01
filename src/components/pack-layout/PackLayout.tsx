877d281104d3f479e4bec3af3811a83b077e1a21
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-06-01 19:27:52 +0200

    Component: PackLayout -  Logic to Layout Cards on desktop

--- src/components/pack-layout/PackLayout.tsx
+++ src/components/pack-layout/PackLayout.tsx
@@ -1,3 +1,46 @@
-export default function PackLayout() {
-  return <div>PackLayout</div>
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useContext, useEffect, useState } from "react"
+import PackOpeningContext from "../../context/PackOpeningContext"
+import { chunk } from "../../utils/chunk"
+import "./style.css"
+import { setInitialState } from "../../utils/setInitialState"
+
+interface Props {
+  cardIds: string[]
+}
+
+export default function PackLayout({ cardIds }: Props) {
+  const [top, setTop] = useState<string[]>([])
+  const [middle, setMiddle] = useState<string[]>([])
+  const [bottom, setBottom] = useState<string[]>([])
+  const largePack = 7
+
+  const { actions } = useContext(PackOpeningContext)!
+
+  const topRow = top?.map((id: string) => <div key={id}></div>)
+  const middleRow = middle?.map((id: string) => <div key={id}></div>)
+  const bottomRow = bottom?.map((id: string) => <div key={id}></div>)
+
+  const desktopList = (
+    <div className="desktop-list">
+      {topRow && <div className="pack-row">{topRow}</div>}
+      {middleRow && <div className="pack-row">{middleRow}</div>}
+      {bottomRow && <div className="pack-row">{bottomRow}</div>}
+    </div>
+  )
+  useEffect(() => {
+    actions.setFlippedState(setInitialState(cardIds))
+  }, [cardIds])
+
+  useEffect(() => {
+    const chunkDivisor = cardIds.length >= largePack ? 3 : 2
+    const chunkSize = Math.ceil(cardIds.length / chunkDivisor)
+    const chunks = chunk(cardIds, chunkSize)
+
+    setTop(chunks[0])
+    setMiddle(chunks[2])
+    setBottom(chunks[1])
+  }, [cardIds])
+
+  return <>{desktopList}</>
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,21 +1,17 @@
-// import { useContext, useEffect } from "react"
 import { useLoaderData } from "react-router-dom"
 import { AbsoluteBg } from "../../components/absolute-bg/AbsoluteBg"
 import { HomeLoaderResult } from "./loader"
-import { useEffect } from "react"
-// import PackOpeningContext from "../../context/PackOpeningContext"
+import PackLayout from "../../components/pack-layout/PackLayout"
+import { ICard } from "../../api/types"
 
 export default function HomePage() {
   const { pack } = useLoaderData() as HomeLoaderResult
-
-  useEffect(() => {
-    console.log(pack)
-  }, [pack])
-  // const { state } = useContext(PackOpeningContext)!
+  const cardIds = pack.cards.map((card: ICard) => card.id)
 
   return (
     <div className="flex min-h-screen flex-col w-screen relative">
       <AbsoluteBg />
+      <PackLayout cardIds={cardIds} />
     </div>
   )
 }
